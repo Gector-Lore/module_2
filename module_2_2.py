fb30e545af67a56375353c34fc8d7f28953cdc84
Gector-Lore <168639156+Gector-Lore@users.noreply.github.com>
2024-12-10 20:29:07 +0500

    Update module_2_2.py
    Убрано лишнее условие в строке 6

--- module_2_2.py
+++ module_2_2.py
@@ -3,7 +3,7 @@ second = (int(input('Второе число: ')))
 third = (int(input('Третье число: ')))
 if first == second == third:
     print('3')
-elif first == second == third or first == second or second == third or first == third:
+elif first == second or second == third or first == third:
     print('2')
 elif first != second != third:
     print('0')
